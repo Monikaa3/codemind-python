725b7385f8dd844707a20eb67e7e5bb7b5e9c439
21MH1A0 595 <97929957+Monikaa3@users.noreply.github.com>
2022-10-04 21:26:20 +0530

    p=input().lower() p=p.split(" ") a=p[0] x='' for i in a:     count = 0     for j in range(1,len(p)):         if i in p[j]:             count+=1     if count==len(p)-1:         x+=i if len(x)==0:     print("-1") else:     print(min(x))

---
+++ Common_characters_-_3.py
@@ -0,0 +1,16 @@
+p=input().lower()
+p=p.split(" ")
+a=p[0]
+x=''
+for i in a:
+    count = 0
+    for j in range(1,len(p)):
+        if i in p[j]:
+            count+=1
+    if count==len(p)-1:
+        x+=i
+if len(x)==0:
+    print("-1")
+else:
+    print(min(x))
+    
