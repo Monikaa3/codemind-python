a88e3a4cb0edaa4b7d40501f9c2c81c649cedc9b
21MH1A0 595 <97929957+Monikaa3@users.noreply.github.com>
2022-10-06 10:47:46 +0530

    def isvalid(s):     opening="{[("     closing="}])"     brackets={')':'(','}':'{',']':'['}     stack=[]
        for char in s:
            if char in opening:
                stack.append(char)
            elif char in closing:
                if len(stack)==0:
                    return False
                if stack[-1]==brackets[char]:
                    stack.pop()
                else:
                    return False
        return len(stack)==0
    n=input()
    c=isvalid(n)
    print(c)

---
+++ Balanced_Parentheses_1.py
@@ -0,0 +1,20 @@
+def isvalid(s):
+    opening="{[("
+    closing="}])"
+    brackets={')':'(','}':'{',']':'['}
+    stack=[]
+    
+    for char in s:
+        if char in opening:
+            stack.append(char)
+        elif char in closing:
+            if len(stack)==0:
+                return False
+            if stack[-1]==brackets[char]:
+                stack.pop()
+            else:
+                return False
+    return len(stack)==0
+n=input()
+c=isvalid(n)
+print(c)
