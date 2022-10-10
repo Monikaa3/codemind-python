8e7174402e585fd64fa0a86903203f5e757f74d1
21MH1A0 595 <97929957+Monikaa3@users.noreply.github.com>
2022-10-10 20:30:19 +0530

    t=int(input()) for i in range(t):     n=int(input())     c=1     a=list(map(int,input().split()))     for i in range(n-1):         if a[i+1]<a[i]:             c+=1     print(c)

---
+++ Speed.py
@@ -0,0 +1,9 @@
+t=int(input())
+for i in range(t):
+    n=int(input())
+    c=1
+    a=list(map(int,input().split()))
+    for i in range(n-1):
+        if a[i+1]<a[i]:
+            c+=1
+    print(c)
