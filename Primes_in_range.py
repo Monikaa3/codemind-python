055799c4b331882cd0bef27c156672868336f2bc
21MH1A0 595 <97929957+Monikaa3@users.noreply.github.com>
2022-10-12 14:07:31 +0530

    def prime(x,y):     count=0     for i in range(x,y+1):         if i>1:             for j in range(2,int(i**0.5)+1):                 if i%j==0:                     break             else:                 count+=1     print(count) x=int(input()) y=int(input()) prime(x,y)

---
+++ Primes_in_range.py
@@ -0,0 +1,13 @@
+def prime(x,y):
+    count=0
+    for i in range(x,y+1):
+        if i>1:
+            for j in range(2,int(i**0.5)+1):
+                if i%j==0:
+                    break
+            else:
+                count+=1
+    print(count)
+x=int(input())
+y=int(input())
+prime(x,y)
