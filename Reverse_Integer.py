b8d8bba4af33857a6d007d3b5c9d5284aeec56ce
21MH1A0 595 <97929957+Monikaa3@users.noreply.github.com>
2022-10-12 15:16:15 +0530

    n=int(input()) if n<0:     n=n*-1     n=str(n)     n=n[::-1]     n=int(n)     print(n*-1) else:     n=str(n)     n=n[::-1]     n=int(n)     print(n)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,13 +1,12 @@
 n=int(input())
-rev=0
-flag=0
 if n<0:
-    n*=-1
-    flag+=1
-while n:
-    r=n%10
-    rev=(rev*10)+r
-    n=n//10
-if flag!=0:
-    rev=-(rev)
-print(rev)
+    n=n*-1
+    n=str(n)
+    n=n[::-1]
+    n=int(n)
+    print(n*-1)
+else:
+    n=str(n)
+    n=n[::-1]
+    n=int(n)
+    print(n)
