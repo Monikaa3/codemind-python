ef90863e4a64558f137334d9093ee7165509de2c
21MH1A0 595 <97929957+Monikaa3@users.noreply.github.com>
2022-10-12 12:07:55 +0530

    def value(r):     if (r=='I'):         return 1     if (r=='V'):         return 5     if (r=='X'):         return 10     if (r=='L'):         return 50     if (r=='C'):         return 100     if (r=='D'):         return 500     if (r=='M'):         return 1000     return -1
    def roman(str):
        res=0
        i=0
        while(i<len(str)):
            s1=value(str[i])
            if (i+1<len(str)):
                s2=value(str[i+1])
                if (s1>=s2):
                    res=res+s1
                    i=i+1
                else:
                    res=res+s2-s1
                    i=i+2
            else:
                res=res+s1
                i=i+1
        return res
    n=input()
    print(roman(n))

--- Roman_to_Integer.py
+++ Roman_to_Integer.py
@@ -1,35 +1,36 @@
 def value(r):
-    if r=='I':
+    if (r=='I'):
         return 1
-    if r=='V':
+    if (r=='V'):
         return 5
-    if r=='X':
+    if (r=='X'):
         return 10
-    if r=='L':
+    if (r=='L'):
         return 50
-    if r=='C':
+    if (r=='C'):
         return 100
-    if r=='D':
+    if (r=='D'):
         return 500
-    if r=='M':
+    if (r=='M'):
         return 1000
     return -1
-def roman(s):
+ 
+def roman(str):
     res=0
     i=0
-    while i<len(s):
-        s1=value(s[i])
-        if i+1<len(s):
-            s2=value(s[i+1])
-            if s1>=s2:
-                res+=s1
-                i+=1
+    while(i<len(str)):
+        s1=value(str[i])
+        if (i+1<len(str)):
+            s2=value(str[i+1])
+            if (s1>=s2):
+                res=res+s1
+                i=i+1
             else:
-                res+=s2-s1
-                i+=2
+                res=res+s2-s1
+                i=i+2
         else:
-            res+=s1
-            i+=1
+            res=res+s1
+            i=i+1
     return res
 n=input()
 print(roman(n))
