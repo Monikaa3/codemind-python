ab5cb6630eeefd3bb721a6cd53137e2b6da89e92
21MH1A0 595 <97929957+Monikaa3@users.noreply.github.com>
2022-10-12 15:08:36 +0530

    n=int(input()) for  i in range(2,(n//2)+1):     if n%i==0:         print("Not Mega Prime")         break else:     c=0     pd=0     while n:         r=n%10         n=n//10         c+=1         if r==3 or r==5 or r==7:             pd+=1     if c==pd:         print("Mega Prime")     else:         print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -1,20 +1,18 @@
-def mega(n):
-    for i in range(2,(n//2)+1):
-        if n%i==0:
-            return("Not Mega Prime")
-            break
-    else:
-        d=0
-        pd=0
-        while n:
-            r=n%10
-            n=n//10
-            d+=1
-            if r==3 or r==2 or r==5 or r==7:
-                pd+=1
-    if d==pd:
-        return("Mega Prime")
-    else:
-        return("Not Mega Prime")
 n=int(input())
-print(mega(n))
+for  i in range(2,(n//2)+1):
+    if n%i==0:
+        print("Not Mega Prime")
+        break
+else:
+    c=0
+    pd=0
+    while n:
+        r=n%10
+        n=n//10
+        c+=1
+        if r==3 or r==5 or r==7:
+            pd+=1
+    if c==pd:
+        print("Mega Prime")
+    else:
+        print("Not Mega Prime")
