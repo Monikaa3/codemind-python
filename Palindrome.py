03d3960e37778e6712633c9c466985c612ab5506
21MH1A0 595 <97929957+Monikaa3@users.noreply.github.com>
2022-10-14 21:40:52 +0530

    n=int(input()) n=str(n) m=n[::-1] if m==n:     print(True) else:     print(False)

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,7 @@
 n=int(input())
-num=n
-rev=0
-while n>0:
-    rem=n%10
-    rev=(rev*10)+rem
-    n=n//10
-if rev==num:
-    print("True")
+n=str(n)
+m=n[::-1]
+if m==n:
+    print(True)
 else:
-    print("False")
+    print(False)
